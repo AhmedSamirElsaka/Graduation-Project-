2906fb4422af2e0c45c7a8976e92bb9736602c31
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-06-20 21:37:55 +0300

    add pdf from internet screen

--- app/src/main/java/com/example/graduationproject/ui/PdfFromInternerActivity.kt
+++ app/src/main/java/com/example/graduationproject/ui/PdfFromInternerActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.graduationproject.databinding.ActivityPdfFromInternerBinding
+import com.example.graduationproject.ui.util.saveTo
 
 class PdfFromInternerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPdfFromInternerBinding
@@ -15,12 +16,26 @@ class PdfFromInternerActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.showOnlinePdf.setOnClickListener {
-            var pdfLink = binding.pdfLinkEt.text
+            var pdfLink = binding.pdfLinkEt.text.toString()
             if(!pdfLink.isNullOrEmpty()){
-
+                launchPdfFromUrl(pdfLink)
             }else {
                 Toast.makeText(applicationContext, "Please Enter Valid Link", Toast.LENGTH_SHORT).show()
             }
         }
+
+
+    }
+    private fun launchPdfFromUrl(url: String) {
+
+        startActivity(
+            PdfViewerActivity.launchPdfFromUrl(
+                context = this,
+                pdfUrl = url,
+                pdfTitle = "PDF Title",
+                saveTo = saveTo.ASK_EVERYTIME,
+                enableDownload = true
+            )
+        )
     }
 }
